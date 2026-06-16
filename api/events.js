70ee0f84434ab75c67f945b3264648051de1a098
bachataroma-arch <bachataroma@gmail.com>
2026-06-16 15:25:49 +0200

    Update events.js

--- api/events.js
+++ api/events.js
@@ -1,10 +1,9 @@
 export default async function handler(req, res) {
-  // 🔥 CORS SEMPRE (anche prima del fetch)
-  res.setHeader("Access-Control-Allow-Origin", "https://events.bachataroma.com");
+  // 🔥 CORS sempre prima di tutto
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  // 🔥 gestisce preflight
   if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
