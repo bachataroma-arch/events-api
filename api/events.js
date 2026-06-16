ddd95c6b59a896e897c354061da98d8dd2a1b62e
bachataroma-arch <bachataroma@gmail.com>
2026-06-16 14:50:18 +0200

    Add API handler for fetching events

---
+++ api/events.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      "https://www.goandance.com/api/affiliates/6b69b3cc-49a3-445e-b6cb-8121d472c0c7/events"
+    );
+
+    const data = await response.json();
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).json(data);
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
