1e80a6268e5b0f1dfc153eab7cba58195f4bacba
bachataroma-arch <bachataroma@gmail.com>
2026-06-16 15:55:18 +0200

    Update events.js

--- api/events.js
+++ api/events.js
@@ -1,5 +1,4 @@
 export default async function handler(req, res) {
-
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
@@ -9,30 +8,19 @@ export default async function handler(req, res) {
   }
 
   try {
-
-    let url = "https://www.goandance.com/api/affiliates/6b69b3cc-49a3-445e-b6cb-8121d472c0c7/events";
-
     let allEvents = [];
 
+    let url =
+      "https://www.goandance.com/api/affiliates/6b69b3cc-49a3-445e-b6cb-8121d472c0c7/events";
+
     while (url) {
-
       const response = await fetch(url);
-
-      if (!response.ok) {
-        throw new Error(`HTTP error ${response.status}`);
-      }
-
       const json = await response.json();
 
       allEvents.push(...(json.data || []));
 
-      const next = json.meta?.nextUrl;
-
-      url = next
-        ? (next.startsWith("http")
-            ? next
-            : new URL(next, "https://www.goandance.com").href)
-        : null;
+      // NEXT PAGE (se esiste)
+      url = json.meta?.nextUrl || null;
     }
 
     return res.status(200).json({
@@ -40,6 +28,8 @@ export default async function handler(req, res) {
     });
 
   } catch (error) {
-    return res.status(500).json({ error: error.message });
+    return res.status(500).json({
+      error: error.message
+    });
   }
 }
